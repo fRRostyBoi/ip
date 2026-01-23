82398a3cb7e464f36a18c944c44b8841cf6fdc58
ThatOneRR <ronaldpangrenda@gmail.com>
2026-01-23 12:16:15 +0800

    Renamed Chatbot, implemented skeletal startup & shutdown sequence

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# NotJippity User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/NotJippity.java
@@ -0,0 +1,18 @@
+public class NotJippity {
+
+    public static void main(String[] args) {
+        printStartupMsg();
+        printExitMsg();
+    }
+
+    private static void printStartupMsg() {
+        System.out.println("____________________________________________________________");
+        System.out.println("[NotJippity] What's up?\n");
+    }
+
+    private static void printExitMsg() {
+        System.out.println("\n[NotJippity] Aite cool, cya.");
+        System.out.println("____________________________________________________________");
+    }
+
+}
